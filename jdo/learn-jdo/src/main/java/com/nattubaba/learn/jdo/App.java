541dd8f2c04997ec37aba3fec86d41908712c554
Nitiraj Singh Rathore <nitiraj.rathore@gmail.com>
2012-06-30 02:04:32 +0530

    adding the jdo project

---
+++ jdo/learn-jdo/src/main/java/com/nattubaba/learn/jdo/App.java
@@ -0,0 +1,13 @@
+package com.nattubaba.learn.jdo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

30f2c8752b01aa4acd483d904cb3041f88a45792
Nitiraj Singh Rathore <nitiraj.rathore@gmail.com>
2012-07-16 17:49:55 +0530

    example maven project

---
+++ maventest/another-project/src/main/java/com/tbitsglobal/another/App.java
@@ -0,0 +1,13 @@
+package com.tbitsglobal.another;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maventest/sample-project/src/test/java/com/tbitsglobal/sample/project/AppTest.java
@@ -0,0 +1,48 @@
+//package com.tbitsglobal.sample.project;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//
+///**
+// * Unit test for simple App.
+// */
+//
+//public class AppTest 
+//{
+//    /**
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//     */
+//
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    @BeforeClass
+//    public static void suite()
+//    {
+//    }
+//
+//    /**
+//     * Rigourous Test :-)
+//     */
+//    @Test
+//    public void testApp()
+//    {
+//        assertTrue( true );
+//    }
+//    
+//    @Test
+//    public void testApp2()
+//    {
+//        assertTrue( true );
+//    }
+//
+//    @Before
+//    public void beforeEveryTest()
+//    {
+//    }
+//}
